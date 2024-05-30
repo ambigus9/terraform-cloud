fee96d01ea1ef54891e7c12d4d1375222d841bdf
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-05-29 19:33:30 -0500

    Adding Variables to connect to AWS

--- providers.tf
+++ providers.tf
@@ -10,11 +10,13 @@ terraform {
 
 provider "aws" {
 
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+
   default_tags {
     tags = var.tags
   }
 
-  region = "us-east-1"
-  #alias  = "virginia"
 
 }
